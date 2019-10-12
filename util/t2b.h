84a5ed87ac0fb2495cafd87a556efd06881a307c
Zhang Rui 张睿 <zrui16@hotmail.com>
2019-10-12 00:40:05 -0700

    Add missing t2b.h

---
+++ util/t2b.h
@@ -0,0 +1,40 @@
+#ifndef T2B_H
+#define T2B_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+struct enumType {
+	char *name;
+	struct enumDesc *vals;
+	struct enumType *next;
+};
+
+struct enumDesc {
+	char *name;
+	uint16_t val;
+	struct enumDesc *next;
+};
+
+struct msgType {
+	char *name;
+	struct fldDesc *flds;
+	struct msgType *next;
+};
+
+struct fldDesc {
+	char *name;
+	char *tname;
+	bool list;
+	struct fldDesc *next;
+};
+
+struct schemaDesc {
+	char *pkgName;
+	struct enumType *enums;
+	struct msgType *msgs;
+};
+
+extern struct schemaDesc schema;
+
+#endif  /* T2B_H */
