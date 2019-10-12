4ccf5b46882fad5fe82c74a51be0c426b8f6abe5
Zhang Rui 张睿 <zrui16@hotmail.com>
2019-10-12 00:45:58 -0700

    Update bench_nb.c to use generated header

--- benchmark/bench_nb.c
+++ benchmark/bench_nb.c
@@ -1,4 +1,4 @@
-#include "bench_nb_generated.h"
+#include "bench.nb.h"
 
 #include <stdio.h>
 #include <inttypes.h>
@@ -7,7 +7,7 @@ extern void _(const char *, ...);
 
 // #define _ printf
 
-static void print_Foo(Foo foo)
+static void print_Foo(Foo *foo)
 {
 	_("{id = %" PRIx64
 	  ", count = %" PRIu16
@@ -17,10 +17,11 @@ static void print_Foo(Foo foo)
 	  Foo_id(foo), Foo_count(foo), Foo_prefix(foo), Foo_length(foo));
 }
 
-static void print_Bar(Bar bar)
+static void print_Bar(Bar *bar)
 {
+	Foo foo = Bar_parent(bar);
 	_("{parent = ");
-	print_Foo(Bar_parent(bar));
+	print_Foo(&foo);
 	_(", time = %" PRId32
 	  ", ratio = %f"
 	  ", size = %" PRIu16
@@ -30,10 +31,11 @@ static void print_Bar(Bar bar)
 	  Bar_size(bar));
 }
 
-static void print_FooBar(FooBar foobar)
+static void print_FooBar(FooBar *foobar)
 {
+	Bar bar = FooBar_sibling(foobar);
 	_("{sibling = ");
-	print_Bar(FooBar_sibling(foobar));
+	print_Bar(&bar);
 	size_t name_len;
 	const char *name = FooBar_name(foobar, &name_len);
 	_(", name = %.*s"
@@ -45,14 +47,14 @@ static void print_FooBar(FooBar foobar)
 	  FooBar_postfix(foobar));
 }
 
-static void print_FooBarContainer(FooBarContainer container)
+static void print_FooBarContainer(FooBarContainer *container)
 {
 	size_t list_size = FooBarContainer_list_size(container);
 	size_t i;
 	_("{list = [");
 	for (i = 0; i < list_size; i++) {
 		FooBar foobar = FooBarContainer_list(container, i);
-		print_FooBar(foobar);
+		print_FooBar(&foobar);
 		_(", ");
 	}
 	size_t location_len;
@@ -70,8 +72,8 @@ static void print_FooBarContainer(FooBarContainer container)
 
 void test_read(struct nbuf_buffer *buf)
 {
-	FooBarContainer container = nbuf_get_FooBarContainer(buf);
-	print_FooBarContainer(container);
+	FooBarContainer container = get_FooBarContainer(buf);
+	print_FooBarContainer(&container);
 	_("\n");
 }
 
@@ -79,26 +81,26 @@ void test_write(struct nbuf_buffer *buf)
 {
 	nbuf_clear(buf);
 	const int veclen = 3;
-	FooBarContainer container = nbuf_init_FooBarContainer(buf);
-	FooBarContainer_init_list(container, veclen);
+	FooBarContainer container = new_FooBarContainer(buf);
+	FooBarContainer_init_list(&container, veclen);
 	for (int i = 0; i < veclen; i++) {
-		FooBar foobar = FooBarContainer_list(container, i);
-		Bar bar = FooBar_init_sibling(foobar);
-		Foo foo = Bar_init_parent(bar);
-		Foo_set_id(foo, 0xABADCAFEABADCAFE + i);
-		Foo_set_count(foo, 10000 + i);
-		Foo_set_prefix(foo, '@' + i);
-		Foo_set_length(foo, 1000000 + i); 
-		Bar_set_time(bar, 123456 + i);
-		Bar_set_ratio(bar, 3.14159f + i);
-		Bar_set_size(bar, 10000 + i);
-		FooBar_set_name(foobar, "Hello, World!", -1);
-		FooBar_set_rating(foobar, 3.1415432432445543543 + i);
-		FooBar_set_postfix(foobar, '!' + i);
+		FooBar foobar = FooBarContainer_list(&container, i);
+		Bar bar = FooBar_init_sibling(&foobar);
+		Foo foo = Bar_init_parent(&bar);
+		Foo_set_id(&foo, 0xABADCAFEABADCAFE + i);
+		Foo_set_count(&foo, 10000 + i);
+		Foo_set_prefix(&foo, '@' + i);
+		Foo_set_length(&foo, 1000000 + i);
+		Bar_set_time(&bar, 123456 + i);
+		Bar_set_ratio(&bar, 3.14159f + i);
+		Bar_set_size(&bar, 10000 + i);
+		FooBar_set_name(&foobar, "Hello, World!", -1);
+		FooBar_set_rating(&foobar, 3.1415432432445543543 + i);
+		FooBar_set_postfix(&foobar, '!' + i);
 	}
-	FooBarContainer_set_initialized(container, true);
-	FooBarContainer_set_fruit(container, Enum_Bananas);
-	FooBarContainer_set_location(container, "http://github.com/", -1);
+	FooBarContainer_set_initialized(&container, true);
+	FooBarContainer_set_fruit(&container, Enum_Bananas);
+	FooBarContainer_set_location(&container, "http://github.com/", -1);
 }
 
 #include "bench.h"
