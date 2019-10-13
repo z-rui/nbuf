f77e03212d611eeeb5f274175a7258ed670516ce
Zhang Rui 张睿 <zrui16@hotmail.com>
2019-10-12 22:22:24 -0700

    Rename b2r to b2c

--- nbufc/b2c.c
+++ nbufc/b2c.c
@@ -1,4 +1,4 @@
-/* Generate header from binary schema. */
+/* Generate C data from binary schema. */
 
 #include "nbuf.nb.h"
 #include "libnbufc.h"
@@ -8,13 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct nbuf_b2r {
+struct nbuf_b2c {
 	nbuf_Schema schema;
 	char *prefix;
 };
 
 static void
-outbuf(struct nbuf_b2r *b2r, FILE *fout, struct nbuf_buffer *buf)
+outbuf(struct nbuf_b2c *b2c, FILE *fout, struct nbuf_buffer *buf)
 {
 	size_t i;
 
@@ -31,90 +31,90 @@ outbuf(struct nbuf_b2r *b2r, FILE *fout, struct nbuf_buffer *buf)
 }
 
 static void
-genenums(struct nbuf_b2r *b2r, FILE *fout)
+genenums(struct nbuf_b2c *b2c, FILE *fout)
 {
-	size_t n = nbuf_Schema_enumTypes_size(&b2r->schema);
+	size_t n = nbuf_Schema_enumTypes_size(&b2c->schema);
 	size_t i;
 
 	for (i = 0; i < n; i++) {
-		nbuf_EnumType e = nbuf_Schema_enumTypes(&b2r->schema, i);
+		nbuf_EnumType e = nbuf_Schema_enumTypes(&b2c->schema, i);
 		const char *ename = nbuf_EnumType_name(&e, NULL);
 
-		fprintf(fout, "\nnbuf_EnumType %sschema_%s = ",
-			b2r->prefix, ename);
+		fprintf(fout, "\nnbuf_EnumType %srefl_%s = ",
+			b2c->prefix, ename);
 		fprintf(fout, "{{&buf, 1, %u, %u}};\n", e.o.ssize, e.o.psize);
 	}
 }
 
 static void
-genmsgs(struct nbuf_b2r *b2r, FILE *fout)
+genmsgs(struct nbuf_b2c *b2c, FILE *fout)
 {
-	size_t n = nbuf_Schema_msgTypes_size(&b2r->schema);
+	size_t n = nbuf_Schema_msgTypes_size(&b2c->schema);
 	size_t i;
 
 	/* 1st pass - declare all types */
 	for (i = 0; i < n; i++) {
-		nbuf_MsgType m = nbuf_Schema_msgTypes(&b2r->schema, i);
+		nbuf_MsgType m = nbuf_Schema_msgTypes(&b2c->schema, i);
 		const char *mname = nbuf_MsgType_name(&m, NULL);
 
-		fprintf(fout, "\nnbuf_MsgType %sschema_%s = ",
-			b2r->prefix, mname);
+		fprintf(fout, "\nnbuf_MsgType %srefl_%s = ",
+			b2c->prefix, mname);
 		fprintf(fout, "{{&buf, %lu, 1, %u, %u}};\n",
 			(unsigned long) m.o.base, m.o.ssize, m.o.psize);
 	}
 }
 
 static void
-outhdr(struct nbuf_b2r *b2r, FILE *fout, const char *srcname)
+outhdr(struct nbuf_b2c *b2c, FILE *fout, const char *srcname)
 {
 	size_t i, len;
-	const char *pkgName = nbuf_Schema_pkgName(&b2r->schema, &len);
+	const char *pkgName = nbuf_Schema_pkgName(&b2c->schema, &len);
 	char ch;
 
 	if (len == 0) {
-		b2r->prefix = strdup("");
+		b2c->prefix = strdup("");
 	} else {
 		/* pkg.name => pkg_name_ */
-		b2r->prefix = malloc(len + 2);
+		b2c->prefix = malloc(len + 2);
 		for (i = 0; i < len; i++) {
 			ch = pkgName[i];
 			if (!isalnum(ch))
 				ch = '_';
-			b2r->prefix[i] = ch;
+			b2c->prefix[i] = ch;
 		}
-		strcpy(b2r->prefix + len, "_");
+		strcpy(b2c->prefix + len, "_");
 	}
 
 	fprintf(fout, "/* Generated from %s.  DO NOT EDIT. */\n\n",
 		srcname);
-	fprintf(fout, "#include \"libnbuf.h\"\n");
+	fprintf(fout, "#include \"nbuf.nb.h\"\n");
 }
 
 static void
-outftr(struct nbuf_b2r *b2r, FILE *fout)
+outftr(struct nbuf_b2c *b2c, FILE *fout)
 {
-	struct nbuf_obj *o = &b2r->schema.o;
-	fprintf(fout, "\nnbuf_Schema %sschema = ", b2r->prefix);
+	struct nbuf_obj *o = &b2c->schema.o;
+	fprintf(fout, "\nnbuf_Schema %srefl_schema = ", b2c->prefix);
 	fprintf(fout, "{{&buf, %lu, 1, %u, %u}};\n",
 		(unsigned long) o->base, o->ssize, o->psize);
 }
 
 static void
-cleanup(struct nbuf_b2r *b2r)
+cleanup(struct nbuf_b2c *b2c)
 {
-	free(b2r->prefix);
+	free(b2c->prefix);
 }
 
 void
-nbuf_b2r(struct nbuf_buffer *buf, FILE *fout, const char *srcname)
+nbuf_b2c(struct nbuf_buffer *buf, FILE *fout, const char *srcname)
 {
-	struct nbuf_b2r b2r;
+	struct nbuf_b2c b2c;
 
-	b2r.schema = nbuf_get_Schema(buf);
-	outhdr(&b2r, fout, srcname);
-	outbuf(&b2r, fout, buf);
-	genenums(&b2r, fout);
-	genmsgs(&b2r, fout);
-	outftr(&b2r, fout);
-	cleanup(&b2r);
+	b2c.schema = nbuf_get_Schema(buf);
+	outhdr(&b2c, fout, srcname);
+	outbuf(&b2c, fout, buf);
+	genenums(&b2c, fout);
+	genmsgs(&b2c, fout);
+	outftr(&b2c, fout);
+	cleanup(&b2c);
 }
